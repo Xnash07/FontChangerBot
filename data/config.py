6843d62186b10d74fe1a886b6e1a919330bd09af
Xnash07 <primeishannn@gmail.com>
2025-10-04 09:53:37 +0530

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,16 @@
+from email.policy import default
+
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Hosting ip manzili
+BACKEND_URL = env.str("BACKEND_URL")  # BACKEND_URL manzili
+ADMIN_TOKEN = env.str("ADMIN_TOKEN")  # Admin tokeni
+
+CHANNELS = env.list("CHANNELS", default=[])  # channels
